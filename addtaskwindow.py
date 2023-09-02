c8d10e7910baf72ec03b41eafda66d6cf72c813d
MThilakshana <99790936+MThilakshana@users.noreply.github.com>
2023-09-02 22:13:01 +0530

    Update addtaskwindow.py

--- addtaskwindow.py
+++ addtaskwindow.py
@@ -1,12 +1,1 @@
-import tkinter as tk 
 
-class addtaskwindow:
-    
-    #create window
-    root = tk.Tk()
-    root.title("Add day task")
-    root.geometry("600x500")
-    
-    
-    root.mainloop()
-    
