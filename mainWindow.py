156c09d9d3a483a1e1e3a0e3b8d06b07b024687f
MThilakshana <99790936+MThilakshana@users.noreply.github.com>
2023-09-03 10:23:52 +0530

    Add files via upload

--- mainWindow.py
+++ mainWindow.py
@@ -1,76 +1,107 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
-class mainWindow:
+import mysql.connector
+from datetime import date
+import calendar
+from time import strftime
     
-    #functions
-    def addbutton():
-        top = Toplevel()
-        top.title('Add or Update Tasks - School Alarm System 1.0')
-        top.geometry("1000x700")
-        top.configure(background="#353b48")
-        title = tk.Label(top,text="Add or Update Tasks",fg="White",bg="#353b48",font="Times 30 bold").pack()
-        select_month = tk.Label(top,text="Select Day",fg="White",bg="#353b48",font="Times 15 bold").place(x=40,y=100)
+#conncet to the database
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = ""
+)
+    
+#create database and table
+cursor = mydb.cursor()
+cursor.execute("CREATE DATABASE if not exists schoolalarmsystem")
+cursor.execute("use schoolalarmsystem")
+cursor.execute("Create table if not exists alarm(day varchar(50),id varchar(10),description varchar(100),time varchar(25),status varchar(20))")
+    
+#functions
+def addbutton():
+    top = Toplevel()
+    top.title('Add or Update Tasks - School Alarm System 1.0')
+    top.geometry("1000x700")
+    top.configure(background="#353b48")
+    title = tk.Label(top,text="Add or Update Tasks",fg="White",bg="#353b48",font="Times 30 bold").pack()
+    select_month = tk.Label(top,text="Select Day",fg="White",bg="#353b48",font="Times 15 bold").place(x=40,y=100)
+    
+def time_clock():
+    string = strftime('%H:%M:%S %p')
+    clock.config(text=string)
+    clock.after(1000, time_clock)
+
+
+#get date and time
+today_date = date.today()
+day = calendar.day_name[today_date.weekday()]
+    
+    
+#create window
+root = tk.Tk()
+root.title("School Alarm System 1.0")
+#set backgroung color
+root.configure(background = "#353b48")
+#set window size
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+root.geometry(f"{screen_width}x{screen_height}")
         
+#add labels
+title = tk.Label(root,text="School Alarm System 1.0",fg="White",bg="#353b48",font="Times 35 bold").pack()
+time = tk.Label(root,text="Time",font="Times 20 bold",bg="#353b48",fg="White")
+time.place(x=1250,y=100)
+clock = tk.Label(root,font = "Times 20 bold", bg = "#353b48", fg = "White")
+clock.place(x=1250, y= 140)
 
-    #create window
-    root = tk.Tk()
-    root.title("School Alarm System 1.0")
-    #set backgroung color
-    root.configure(background = "#353b48")
-    #set window size
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-    root.geometry(f"{screen_width}x{screen_height}")
-        
-    #add labels
-    title = tk.Label(root,text="School Alarm System 1.0",fg="White",bg="#353b48",font="Times 35 bold").pack()
+#add buttons
+add_task = tk.Button(root,text="Add Task",font="Times 15 bold",bg="#273c75",fg="White",width=20,height=2,cursor="hand2",command=addbutton)
+add_task.place(x=50,y=100)
 
-    #add buttons
-    add_task = tk.Button(root,text="Add Task",font="Times 15 bold",bg="#273c75",fg="White",width=20,height=2,cursor="hand2",command=addbutton)
-    add_task.place(x=50,y=100)
+delete_task = tk.Button(root,text="Delete Task",fg="White",font="Times 15 bold",bg="#273c75",width=20,height=2,cursor="hand2")
+delete_task.place(x=50,y=200)
 
-    delete_task = tk.Button(root,text="Delete Task",fg="White",font="Times 15 bold",bg="#273c75",width=20,height=2,cursor="hand2")
-    delete_task.place(x=50,y=200)
+exit = tk.Button(root,text="Exit",fg="White",font="Times 15 bold",bg="#273c75",width=20,height=2,cursor="hand2", command=root.destroy)
+exit.place(x=50,y=700)
 
-    exit = tk.Button(root,text="Exit",fg="White",font="Times 15 bold",bg="#273c75",width=20,height=2,cursor="hand2", command=root.destroy)
-    exit.place(x=50,y=682.5)
-
-    #add table
-    my_tree = ttk.Treeview(root,height=15)
-    #define table columns
-    my_tree['columns']=("Description","Time","Status")
-    #set fonts
-    custom_font = ("Times",20)
-    my_tree.tag_configure("custom_font",font=custom_font)
-    #set styles
-    s = ttk.Style()
-    s.theme_use('clam')
-    s.configure('Treeview.Heading',background="Light Blue",font="Times 20")
-    s.configure('Treeview',rowheight=40)
-    #format our columns
-    my_tree.column("#0",width=100,minwidth=25)
-    my_tree.column("Description",anchor=W,width=400)
-    my_tree.column("Time",anchor=CENTER,width=200)
-    my_tree.column("Status",anchor=CENTER,width=200)
-    #create headdings
-    my_tree.heading("#0",text="ID",anchor=CENTER)
-    my_tree.heading("Description",text="Description",anchor=CENTER)
-    my_tree.heading("Time",text="Time",anchor=CENTER)
-    my_tree.heading("Status",text="Status",anchor=CENTER)
-    #add data
-    my_tree.insert(parent='',index='end',iid=0,text='01',values=('1st Period','7.30','No'),tags=("custom_font"))
-    my_tree.insert(parent='',index='end',iid=1,text='02',values=('2nd Period','8.10','No'),tags=("custom_font"))
-    my_tree.insert(parent='',index='end',iid=2,text='03',values=('3rd Period','8.50','No'),tags=("custom_font"))
-    my_tree.insert(parent='',index='end',iid=3,text='04',values=('4th Period','9.30','No'),tags=("custom_font"))
-    my_tree.insert(parent='',index='end',iid=4,text='05',values=('5th Period','10.10','No'),tags=("custom_font"))
-    my_tree.insert(parent='',index='end',iid=5,text='06',values=('Interval','10.50','No'),tags=("custom_font"))
-    my_tree.insert(parent='',index='end',iid=6,text='07',values=('6th Period','11.30','No'),tags=("custom_font"))
-    my_tree.insert(parent='',index='end',iid=7,text='08',values=('7th Period','12.10','No'),tags=("custom_font"))
-    my_tree.insert(parent='',index='end',iid=8,text='09',values=('8th Period','12.50','No'),tags=("custom_font"))
+#add table
+my_tree = ttk.Treeview(root,height=13)
+#define table columns
+my_tree['columns']=("Description","Time","Status")
+#set fonts
+custom_font = ("Times",20)
+my_tree.tag_configure("custom_font",font=custom_font)
+#set styles
+s = ttk.Style()
+s.theme_use('clam')
+s.configure('Treeview.Heading',background="Light Blue",font="Times 20")
+s.configure('Treeview',rowheight=40)
+#format our columns
+my_tree.column("#0",width=100,minwidth=25)
+my_tree.column("Description",anchor=W,width=400)
+my_tree.column("Time",anchor=CENTER,width=200)
+my_tree.column("Status",anchor=CENTER,width=200)
+#create headdings
+my_tree.heading("#0",text="ID",anchor=CENTER)
+my_tree.heading("Description",text="Description",anchor=CENTER)
+my_tree.heading("Time",text="Time",anchor=CENTER)
+my_tree.heading("Status",text="Status",anchor=CENTER)
+#add data
+my_tree.insert(parent='',index='end',iid=0,text='01',values=('1st Period','7.30','No'),tags=("custom_font"))
+my_tree.insert(parent='',index='end',iid=1,text='02',values=('2nd Period','8.10','No'),tags=("custom_font"))
+my_tree.insert(parent='',index='end',iid=2,text='03',values=('3rd Period','8.50','No'),tags=("custom_font"))
+my_tree.insert(parent='',index='end',iid=3,text='04',values=('4th Period','9.30','No'),tags=("custom_font"))
+my_tree.insert(parent='',index='end',iid=4,text='05',values=('5th Period','10.10','No'),tags=("custom_font"))
+my_tree.insert(parent='',index='end',iid=5,text='06',values=('Interval','10.50','No'),tags=("custom_font"))
+my_tree.insert(parent='',index='end',iid=6,text='07',values=('6th Period','11.30','No'),tags=("custom_font"))
+my_tree.insert(parent='',index='end',iid=7,text='08',values=('7th Period','12.10','No'),tags=("custom_font"))
+my_tree.insert(parent='',index='end',iid=8,text='09',values=('8th Period','12.50','No'),tags=("custom_font"))
     
-    #pack to the screen
-    my_tree.place(x=550,y=100)
+#pack to the screen
+my_tree.place(x=550,y=200)
     
+time_clock()
 
-    root.mainloop()
+root.mainloop()
